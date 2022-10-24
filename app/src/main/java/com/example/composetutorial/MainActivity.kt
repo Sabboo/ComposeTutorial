e25b50d97e0fe074b9ae6f96f4030dbb0171aa87
Ahmad.Saber <sander.sader@gmail.com>
2022-10-24 14:42:52 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/composetutorial/MainActivity.kt
@@ -0,0 +1,130 @@
+package com.example.composetutorial
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composetutorial.ui.theme.ComposeTutorialTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeTutorialTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Conversation(
+                        messages = listOf(
+                            Message("Name 1", "This is message content with extra content to preview 1"),
+                            Message("Name 2", "This is message content with extra content to preview 2"),
+                            Message("Name 3", "This is message content with extra content to preview 3"),
+                            Message("Name 4", "This is message content with extra content to preview 4"),
+                            Message("Name 5", "This is message content with extra content to preview 5"),
+                            Message("Name 6", "This is message content with extra content to preview 6"),
+                            Message("Name 7", "This is message content with extra content to preview 7"),
+                            Message("Name 8", "This is message content with extra content to preview 8"),
+                            Message("Name 9", "This is message content with extra content to preview 9")
+                        )
+                    )
+                }
+            }
+        }
+    }
+}
+
+data class Message(val author: String, val body: String)
+
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message = message)
+        }
+    }
+}
+
+@Composable
+fun MessageCard(message: Message) {
+    Row(Modifier.padding(8.dp)) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_baseline_1k_plus_24),
+            contentDescription = stringResource(id = R.string.dsc_user_image),
+            modifier = Modifier
+                .size(48.dp)
+                .clip(CircleShape)
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = message.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                color = surfaceColor,
+                modifier = Modifier
+                    .animateContentSize()
+                    .padding(1.dp)
+            ) {
+                Text(
+                    text = message.body,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(8.dp),
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ComposeTutorialTheme {
+        Conversation(
+            messages = listOf(
+                Message("Name 1", "This is message content with extra content to preview 1"),
+                Message("Name 2", "This is message content with extra content to preview 2"),
+                Message("Name 3", "This is message content with extra content to preview 3"),
+                Message("Name 4", "This is message content with extra content to preview 4"),
+                Message("Name 5", "This is message content with extra content to preview 5"),
+                Message("Name 6", "This is message content with extra content to preview 6"),
+                Message("Name 7", "This is message content with extra content to preview 7"),
+                Message("Name 8", "This is message content with extra content to preview 8"),
+                Message("Name 9", "This is message content with extra content to preview 9")
+            )
+        )
+    }
+}
